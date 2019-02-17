b4c3d0dd635ef708ce6e77f6bcf0641548ec6cb4
landmine007 <651780202@qq.com>
2019-02-17 11:53:18 +0800

    1

--- js/login.js
+++ js/login.js
@@ -1,15 +1,31 @@
 var login = (function(){
     // 获取dom
-    var selNation = document.querySelector('.sel-nation');
+    var $selNation = $('.sel-nation');
+    var $nation = $('.nation-list');
+    var $phone = $('.tele input');
     return {
         init: function(){
             this.event();
         },
         event(){
             //国家选择
-            selNation.onclick = function(){
-                this.nextElementSibling.style.display = 'block';
-            }
+            $selNation.on("click", function(){
+                $(this).next().css("display", 'block');
+            })
+            $nation.on('click', 'li', function(){
+                $selNation.children().eq(0).text($(this).text());
+                $selNation.next().css("display", 'none');
+            });
+            // 手机号码验证
+            $phone.on('blur', function() {
+                console.log(1);
+                var reg = /^1[34578]\d{9}$/;
+                if(reg.test($(this).val())){
+                    $(this).parent().css('borderColor', 'red')
+                           .next().css("display", 'block')
+                           .html('手机号码格式不正确');
+                }
+            });
         }
     }
 }());
