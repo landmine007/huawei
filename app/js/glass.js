21fb631917ceaf42d5b0c3ebcd55f56dd93256c9
lcs666 <487591655@qq.com>
2019-02-20 15:32:03 +0800

    不完美的放大镜

---
+++ app/js/glass.js
@@ -0,0 +1,39 @@
+$(function(){
+	var moveX,moveY;
+	$('li img').bind('mouseover',change);
+//	划片移动
+	$('.big img').bind('mousemove',zoon);
+	$('.zoon').bind('mousemove',zoon);
+//	$('.zoon').show();
+//	$('.big_r').bind('mousemove',big_r);
+//	$('.big_r').show();
+	
+	function zoon(e){
+	if(e.pageY-50>=0 && e.pageY+50<=450)
+		moveY=e.pageY-50;
+//		鼠标中间位置,边界处理
+		if(e.pageX-50>=0 && e.pageX+50<=450)
+		moveX=e.pageX-50;
+		$(".zoon").css({top:moveY,left:moveX});
+	$('.big_r img').css({top:-moveY*.9,left:-moveX*.9})
+		
+		
+	}
+//	切换小图片
+	function change(){
+	$("li img").css({borderColor:'#fff'});
+	$(this).css({borderColor:'red'});
+	var $src=$(this).prop('src');
+	var $big_r=$src.split('ul_');
+//	console.log($big_r);
+//	console.log($big_r.join(''));
+//	$('.big_r img').prop('src')
+	$('.big img:first').prop('src',$big_r.join(''));
+	$('.big_r img:first').prop('src',$big_r.join(''));
+	}
+});
+//$('.big img').bind('mouseout',zoon);
+//	$('.zoon').bind('mouseout',zoon);
+//	$('.zoon').hide();
+//	$('.big_r').bind('mouseout',big_r);
+//	$('.big_r').hide();
